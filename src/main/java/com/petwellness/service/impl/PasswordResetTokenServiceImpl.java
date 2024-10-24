7150053a87774d581ec2736d5db83aab1d352ace
Marsi3116 <marsi3116@gmail.com>
2024-10-24 15:03:11 -0500

    feat:Implementacion de servicio PasswordResetToken

---
+++ src/main/java/com/petwellness/service/impl/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,87 @@
+package com.petwellness.service.impl;
+
+import com.petwellness.exception.ResourceNotFoundException;
+import com.petwellness.integration.notification.email.dto.Mail;
+import com.petwellness.integration.notification.email.service.EmailService;
+import com.petwellness.model.entity.PasswordResetToken;
+import com.petwellness.model.entity.User;
+import com.petwellness.repository.PasswordResetTokenRepository;
+import com.petwellness.repository.UsuarioRepository;
+import com.petwellness.service.PasswordResetTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+    private final UsuarioRepository userRepository; //vre si se cambia
+    private final EmailService emailService;
+
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+
+    @Transactional
+    @Override
+    public void creatAndSendPasswordResetToken(String email) throws Exception {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado conn email: " + email));
+        PasswordResetToken passwordResetToken = new PasswordResetToken();
+        passwordResetToken.setToken(UUID.randomUUID().toString());
+        passwordResetToken.setUser(user);
+        passwordResetToken.setExpiration(10);
+        passwordResetTokenRepository.save(passwordResetToken);
+
+        Map<String, Object> model = new HashMap<>();
+        String resetUrl = "http://localhost:4200/#/forgot/" + passwordResetToken.getToken();
+        model.put("user", user.getEmail());
+        model.put("resetUrl", resetUrl);
+
+        Mail mail = emailService.createMail(
+                user.getEmail(),
+                "Restablecer Contraseña",
+                model,
+                mailFrom
+        );
+
+        emailService.sendMail(mail,"email/password-reset-template");
+
+    }
+
+    @Override
+    public PasswordResetToken findByToken(String token) {
+        return passwordResetTokenRepository.findByToken(token)
+                .orElseThrow(() -> new ResourceNotFoundException("Token de restablecimiento de contraseña no encontrado"));
+    }
+
+    @Override
+    public void removeResetToken(PasswordResetToken passwordResetToken) {
+        passwordResetTokenRepository.delete(passwordResetToken);
+
+    }
+
+    @Override
+    public boolean isValidToken(String token) {
+        return passwordResetTokenRepository.findByToken(token)
+                .filter(t->!t.isExpired())
+                .isPresent();
+    }
+
+    @Override
+    public void resetPassword(String token, String newPassword) {
+        PasswordResetToken resetToken = passwordResetTokenRepository.findByToken(token)
+                .filter(t->!t.isExpired())
+                .orElseThrow(() -> new ResourceNotFoundException("Token invalido o expirado"));
+
+        User user = resetToken.getUser();
+        user.setContrasena(newPassword);
+        userRepository.save(user);
+        passwordResetTokenRepository.delete(resetToken);
+    }
+}
