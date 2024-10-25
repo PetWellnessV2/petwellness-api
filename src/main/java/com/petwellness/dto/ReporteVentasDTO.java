6fd68e14f60f0d8b77fb12cb3c72bb6a4208ce07
JayGhz <jaykansas7@gmail.com>
2024-10-24 20:32:49 -0500

    refactor(dto): arreglando error en ReporteVentasDTO

--- src/main/java/com/petwellness/dto/ReporteVentasDTO.java
+++ src/main/java/com/petwellness/dto/ReporteVentasDTO.java
@@ -1,6 +1,5 @@
 package com.petwellness.dto;
 
-import com.petwellness.model.enums.TipoProducto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +16,6 @@ public class ReporteVentasDTO {
     private BigDecimal precioTotal;
     private String nombreProducto;
     private BigDecimal costo;
-    private TipoProducto tipoProducto;
+    private String categoriaProducto;
     private String descripcion;
 }
