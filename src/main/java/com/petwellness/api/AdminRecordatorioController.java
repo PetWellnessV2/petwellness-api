937717ebc2470b85261374469875f6bebea1f568
DiegoEBV <diegoballonvi@gmail.com>
2024-11-07 12:45:15 -0500

    fix: correccion en controller de recordatorios

--- src/main/java/com/petwellness/api/AdminRecordatorioController.java
+++ src/main/java/com/petwellness/api/AdminRecordatorioController.java
@@ -1,21 +1,33 @@
 package com.petwellness.api;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.petwellness.dto.RecordatorioDTO;
+import com.petwellness.model.enums.RecordatorioStatus;
 import com.petwellness.service.AdminRecordatorioService;
 import com.petwellness.service.RecordatorioAutoGeneracionService;
 import lombok.RequiredArgsConstructor;
+import com.petwellness.model.entity.Recordatorio;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/recordatorio")
-@PreAuthorize("hasAnyRole('ADMIN','CUSTOMER')")
 public class AdminRecordatorioController {
     private final AdminRecordatorioService adminRecordatorioService;
     private final RecordatorioAutoGeneracionService recordatorioAutoGeneracionService;
-    /*
+
     @GetMapping
     public ResponseEntity<List<Recordatorio>> getAllRecordatorios() {
         return ResponseEntity.ok(adminRecordatorioService.getAll());
@@ -25,11 +37,11 @@ public class AdminRecordatorioController {
     public ResponseEntity<Page<Recordatorio>> paginateRecordatoriosByUsuario(
             @PathVariable("usuarioId") Integer usuarioId,
             @PageableDefault(size = 5, sort = "fechaHora", direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<Recordatorio> recordatorios = adminRecordatorioService.paginateByUsuarioId(usuarioId, pageable);
+        Page<Recordatorio> recordatorios = adminRecordatorioService.paginateByUserId(usuarioId, pageable);
         return new ResponseEntity<>(recordatorios, HttpStatus.OK);
     }
 
-    @GetMapping("/usuario/{usuarioId}")
+    /*@GetMapping("/usuario/{usuarioId}")
     public ResponseEntity<List<Recordatorio>> getRecordatoriosByUsuario(@PathVariable("usuarioId") Integer usuarioId) {
         List<Recordatorio> recordatorios = adminRecordatorioService.findByUsuarioId(usuarioId);
         return new ResponseEntity<>(recordatorios, HttpStatus.OK);
@@ -45,7 +57,7 @@ public class AdminRecordatorioController {
     public ResponseEntity<List<Recordatorio>> getUnsentRecordatorios(@PathVariable("usuarioId") Integer usuarioId) {
         List<Recordatorio> recordatorios = adminRecordatorioService.findByUsuarioIdAndStatus(usuarioId, RecordatorioStatus.CREADO);
         return new ResponseEntity<>(recordatorios, HttpStatus.OK);
-    }
+    }*/
 
     @PostMapping
     public ResponseEntity<RecordatorioDTO> createRecordatorio(@RequestBody RecordatorioDTO recordatorioDTO) {
@@ -59,13 +71,13 @@ public class AdminRecordatorioController {
         return new ResponseEntity<>(recordatorioActualizado, HttpStatus.OK);
     }
 
-    @DeleteMapping("/usuario/{usuarioId}/recordatorio/{recordatorioId}")
+    /*@DeleteMapping("/usuario/{usuarioId}/recordatorio/{recordatorioId}")
     public ResponseEntity<Void> deleteRecordatorioByIdAndUsuario(
             @PathVariable Integer usuarioId,
             @PathVariable Integer recordatorioId) {
         adminRecordatorioService.deleteRecordatorioByIdAndUsuarioId(recordatorioId, usuarioId);
         return ResponseEntity.noContent().build();
-    }
+    }*/
 
     @DeleteMapping("/{recordatorioId}")
     public ResponseEntity<Void> deleteRecordatorio(@PathVariable Integer recordatorioId) {
@@ -73,11 +85,11 @@ public class AdminRecordatorioController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/usuario/{usuarioId}")
+    /*@DeleteMapping("/usuario/{usuarioId}")
     public ResponseEntity<Void> deleteRecordatoriosByUsuario(@PathVariable Integer usuarioId) {
         adminRecordatorioService.deleteRecordatoriosByUsuarioId(usuarioId);
         return ResponseEntity.noContent().build();
-    }
+    }*/
 
     @GetMapping("/{id}")
     public ResponseEntity<RecordatorioDTO> getRecordatorioById(@PathVariable Integer id) {
@@ -91,5 +103,5 @@ public class AdminRecordatorioController {
         return ResponseEntity.ok("Recordatorios generados correctamente.");
     }
 
-     */
+
 }
